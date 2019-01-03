4b9e5b91cd96424ba9ccb96d6502c6432b67d1ec
Joe Leonard <josephleonardumt@gmail.com>
2019-01-02 17:14:00 -0800

    add first project

---
+++ WelcomeScreen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WelcomeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
