84435ed146c2ddd4b4e4e093409ce8d20cdd8308
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2019-01-23 10:52:46 -0800

    Add string exercise

---
+++ StringExercise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExercise
+{
+    class StringExercise
+    {
+        static void Main(string[] args)
+        {
+            string a = "Di";
+            string b = "n";
+            string c = "go";
+
+            Console.WriteLine(a + b + c);
+
+            string word = a + b + c;
+
+            Console.WriteLine(word.ToUpper());
+
+            StringBuilder paragraph = new StringBuilder();
+
+            paragraph.Append("\tHello. I am going to write a paragraph.\n");
+            paragraph.Append("This will not be very long,\n");
+            paragraph.Append("but long enough to be considered a paragraph.\n");
+            paragraph.Append("I like mangos.\n");
+            paragraph.Append("I also like strawberries.\n");
+            paragraph.Append("I wish I had strawberries at home,\n");
+            paragraph.Append("but I only have hamburger helper.");
+
+            Console.WriteLine(paragraph);
+
+            Console.Read();
+        }
+    }
+}
