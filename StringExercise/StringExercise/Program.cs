023c2e6835ead3dab3d33e766f6ee0d691fa72f8
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2019-01-23 11:06:20 -0800

    Add proper files

---
+++ StringExercise/StringExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExercise
+{
+    class StringExercise
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ WhileAndDoWhile/WhileAndDoWhile/WhileAndDoWhile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WhileAndDoWhile
+{
+    class WhileAndDoWhile
+    {
+        static void Main(string[] args)
+        {
+            int numOne = 0;
+            int numTwo = 0;
+
+            do
+            {
+                Console.WriteLine("Please enter the same number twice:");
+
+                Console.WriteLine("Number 1:");
+                numOne = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Number 2:");
+                numTwo = Convert.ToInt32(Console.ReadLine());
+            }
+            while (numOne != numTwo);
+
+            Console.WriteLine("Thank you for following instructions.");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("What is the capital of Montana?");
+            string capital = Console.ReadLine().ToLower();
+            string answer = "helena";
+            bool correct = false;
+
+            while (!correct)
+            {
+                Console.WriteLine("Try Again!");
+                capital = Console.ReadLine().ToLower();
+                correct = capital == answer;
+            }
+            Console.WriteLine("You are correct.");
+            Console.Read();
+        }
+    }
+}
