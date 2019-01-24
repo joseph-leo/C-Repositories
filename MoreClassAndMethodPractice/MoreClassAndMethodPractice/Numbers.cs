ce66555014122dfcbe8d995abae4c2e6334a81d2
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2019-01-24 12:59:06 -0800

    More class and method practice

---
+++ MoreClassAndMethodPractice/MoreClassAndMethodPractice/Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClassAndMethodPractice
+{
+    class Numbers
+    {
+        public static void Nums(int x, int y)
+        {
+            x = x + 20;
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ MoreClassAndMethodPractice/MoreClassAndMethodPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClassAndMethodPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers.Nums(3, 4);
+            Numbers.Nums(x: 5, y: 9);
+
+            Console.Read();
+        }
+    }
+}
