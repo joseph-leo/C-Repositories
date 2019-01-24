2dc03d0f4a80ae09e84bee283e142c60f9047004
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2019-01-24 12:10:22 -0800

    Add class and method exercise

---
+++ ClassAndMethods/ClassAndMethods/MathOps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    class MathOps
+    {
+        public static int SubtractTwo(int x)
+        {
+            int total = x - 2;
+            return total;
+        }
+        public static int MultiplyByFour(int x)
+        {
+            int total = x * 4;
+            return total;
+        }
+        public static int PowerOfTwo(int x)
+        {
+            int total = x ^ 2;
+            return total;
+        }
+    }
+}
