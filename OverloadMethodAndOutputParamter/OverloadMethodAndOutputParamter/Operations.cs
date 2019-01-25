9140d7356f5c8d760a681c3e10cf9db9602dbf54
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2019-01-24 18:33:04 -0800

    add overload method and out parameter exercise

---
+++ OverloadMethodAndOutputParamter/OverloadMethodAndOutputParamter/Operations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethodAndOutputParamter
+{
+    public static class Operations
+    {
+        public static void DivideByTwo(int x)
+        {
+            x = x / 2;
+            Console.WriteLine("~" + x);
+        }
+        public static void DivideByTwo(int x, out int y)
+        {
+            y = x / 2;
+            Console.WriteLine("~" + y);
+        }
+    }
+}
---
+++ OverloadMethodAndOutputParamter/OverloadMethodAndOutputParamter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethodAndOutputParamter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to divide by 2.");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Operations.DivideByTwo(x);
+
+            Operations.DivideByTwo(x, out int y);
+
+            Console.Read();
+        }
+        
+    }
+}
