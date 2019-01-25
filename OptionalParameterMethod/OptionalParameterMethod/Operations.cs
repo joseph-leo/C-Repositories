77469bf6d42fbc72063f0beabeb025d99af2be2b
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2019-01-24 17:00:28 -0800

    Add optional method parameter exercise

---
+++ OptionalParameterMethod/OptionalParameterMethod/Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethod
+{
+    class Operations
+    {
+        public static int Multiply(int x, int y = 8)
+        {
+            int total = x * y;
+            return total;
+        }
+    }
+}
---
+++ OptionalParameterMethod/OptionalParameterMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameterMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number to multiply by the first.\nYou can just press enter and the default will be 8.");
+            string yInput = Console.ReadLine();
+            int y;
+
+            if (yInput.Length > 0)
+            {
+                try
+                {
+                    y = Convert.ToInt32(yInput);
+                    Console.WriteLine(Operations.Multiply(x, y));
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine(Operations.Multiply(x));
+            }
+
+            Console.Read();
+        }
+    }
+}
