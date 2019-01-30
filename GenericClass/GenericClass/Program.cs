8f3e06159ffd3b9543837c999fc36f88c670e6b7
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2019-01-30 11:33:49 -0800

    Add generic class example

---
+++ GenericClass/GenericClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> empString = new Employee<string>();
+            empString.Things = new List<string>() { "Hi", "Joe", "no" };
+            List<string> stringList = empString.Things;
+
+            Employee<int> empInt = new Employee<int>();
+            empInt.Things = new List<int>() { 2, 4, 6, 8 };
+            List<int> intList = empInt.Things;
+
+
+
+            foreach (string word in stringList)
+            {
+                Console.WriteLine(word);
+            }
+
+            foreach (int num in intList)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.Read();
+        }
+    }
+}
