a38a4eef9233c5ef3633ab8831d0655bb4a752c7
Joe Leonard <josephleonardumt@gmail.com>
2019-02-07 15:21:41 -0800

    Add example of a constant, declaring a variable with var, and chaining constructors.

---
+++ ConstVarAndChainConstructors/ConstVarAndChainConstructors/Division.cs
@@ -0,0 +1,16 @@
+﻿namespace ConstVarAndChainConstructors
+{
+    public class Division
+    {
+        public Division(double dividend) : this(dividend, 6)
+        {
+        }
+        public Division(double dividend, double divisor)
+        {
+            Dividend = dividend;
+            Divisor = divisor;
+        }
+        public double Dividend { get; set; }
+        public double Divisor { get; set; }
+    }
+}
---
+++ ConstVarAndChainConstructors/ConstVarAndChainConstructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConstVarAndChainConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double divisor = 6.0;
+            Console.WriteLine("Enter a number to divide by 6.");
+            double dividend = Convert.ToInt32(Console.ReadLine());
+
+            Division division1 = new Division(dividend, divisor);
+            Division division2 = new Division(dividend);
+
+            var firstTotal = division1.Dividend / division1.Divisor;
+            var secondTotal = division2.Dividend / division2.Divisor;
+
+            Console.WriteLine(firstTotal);
+            Console.WriteLine(secondTotal);
+
+            Console.Read();
+        }
+    }
+}
