e7334a884ba06e918e61d17b00b29116216687b0
Joe Leonard <josephleonardumt@gmail.com>
2019-02-07 17:07:12 -0800

    Add additional try/catch examples including custom exceptions

---
+++ TryCatchContinued/TryCatchContinued/AgeOfZeroException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchContinued
+{
+    public class AgeOfZeroException : Exception
+    {
+        public AgeOfZeroException()
+            : base() { }
+        public AgeOfZeroException(string message)
+            : base(message) { }
+    }
+}
