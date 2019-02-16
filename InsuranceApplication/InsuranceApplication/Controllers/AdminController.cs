513121281395d19e263e012016be04f8beb2f8b7
joseph-leo <43657372+joseph-leo@users.noreply.github.com>
2019-02-16 07:43:08 -0800

    Add MVC app with Db connection

---
+++ InsuranceApplication/InsuranceApplication/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using InsuranceApplication.Models;
+using InsuranceApplication.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InsuranceApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (CarInsuranceEntities db = new CarInsuranceEntities())
+            {
+
+                
+                var adminVms = new List<AdminVm>();
+
+                var adminData = (from cust in db.Customers
+                           join quote in db.Quotes on cust.Id equals quote.CustomerId
+                           select new
+                           {
+                               cust.FirstName,
+                               cust.LastName,
+                               cust.EmailAddress,
+                               quote.QuoteAmount
+                           }).ToList();
+                foreach (var entry in adminData)
+                {
+                    var adminVm = new AdminVm
+                    {
+                        FirstName = entry.FirstName,
+                        LastName = entry.LastName,
+                        EmailAddress = entry.EmailAddress,
+                        Quote = Convert.ToDecimal(entry.QuoteAmount)
+                    };
+                    adminVms.Add(adminVm);
+                }
+
+                return View(adminVms);
+            }
+        }
+    }
+}
