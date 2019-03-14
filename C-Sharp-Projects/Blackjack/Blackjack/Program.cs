4dd336fb58dafa68d1a2649130b4b335f900bb45
Joe Leonard <josephleonardumt@gmail.com>
2019-03-14 14:52:15 -0700

    Add C# projects

---
+++ C-Sharp-Projects/Blackjack/Blackjack/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome. What's your name");
+            string playerName = Console.ReadLine();
+
+            Console.WriteLine("And how much money did you bring today");
+            int bank = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Hello, {0}. Would you like to join a game of Blackjack?", playerName);
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new BlackjackGame();
+                game += player;
+                player.IsActivelyPlaying = true;
+                while (player.IsActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Have a nice day!");
+
+            Console.Read();
+        }
+    }
+}
